581b309f1aacf52c829f7fa61151edf0b1fe28c9
Karthick Nataraj <104karthick@gmail.com>
2022-09-16 12:19:55 +0530

    Done

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -1,5 +1,38 @@
 package org.sam;
 
 public class Sample {
+	
+	private void pvt() {
+		System.out.println("gg");
 
+	}
+
+	public static void main(String[] args) {
+	
+		String s;
+		Sample s1;
+		
+		
+		
+		
+		
+		
+		
+		
+	/*	="s5i4a3s21";
+		for (int i = 0; i <s.length(); i++) {
+			char c = s.charAt(i);
+			if (Character.isDigit(c)) {
+				System.out.print(c);
+				
+			} 
+			else {
+				System.out.println(c);
+			}
+			
+		}*/
+		
+		
+		
+	}
 }
